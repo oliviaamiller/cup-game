3b316d892caf94d0f2add6dc46af24f00724e8f3
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-11-29 17:31:55 -0800

    added hmtl, css styles, and console logged DOM elements from JS

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 // import functions and grab DOM elements
+const cupOne = document.getElementById('cup-one');
+const cupTwo = document.getElementById('cup-two');
+const cupThree = document.getElementById('cup-three');
+const buttonOne = document.getElementById('button-one');
+const buttonTwo = document.getElementById('button-two');
+const buttonThree = document.getElementById('button-three');
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
+
+console.log(
+  cupOne,
+  cupTwo,
+  cupThree,
+  buttonOne,
+  buttonTwo,
+  buttonThree,
+  winsEl,
+  lossesEl,
+  totalEl,);
 
 // let state
+
+let wins = 0;
+let losses = 0;
 
 // set event listeners 
   // get user input
